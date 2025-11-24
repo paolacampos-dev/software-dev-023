e22107b327fa0e29cee326ae2ff0d7281a6d6433
Manuel <manugonzalez.tech@gmail.com>
2025-11-24 09:51:10 +0000

    feat: add arrays demo

---
+++ demos/week-02/01-js-arrays/app.js
@@ -0,0 +1,28 @@
+//arrays
+
+//we use arrays to sotre multiple values at a time
+
+// structure
+
+// let arrayName = ["value", "value", "value"]
+
+//examples
+
+let myFavouriteFood = ["burritos", "pizza", "sushi"];
+
+let myFavouriteThings = ["sun", 46, true, "sea"];
+
+//array properties
+//index --> a number that indicates the position of a specific item in an array, starting with zero 0
+// - in myFavouriteFood, "burritos" is index 0
+myFavouriteFood[0];
+// - in myFavouriteThings, 46 is index 1
+myFavouriteThings[1];
+console.log(myFavouriteThings[3]);
+//length --> a number that tells us the total amount of items in an array
+// length DOESN'T start at zero 0
+// the length of myFavouriteFood is 3
+myFavouriteFood.length;
+console.log(myFavouriteFood.length); // 3
+myFavouriteThings.length;
+console.log(myFavouriteThings.length); // 4
