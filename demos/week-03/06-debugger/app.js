7f32a2030ef3baf4aad24e2f0e6dd10f8781f98f
Manuel <manugonzalez.tech@gmail.com>
2025-12-03 11:48:00 +0000

    feat: add debugger demo

---
+++ demos/week-03/06-debugger/app.js
@@ -0,0 +1,12 @@
+const randomNumber = 78;
+
+//TODO: count to ten
+
+function countToTen() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    debugger; //add a breakpoint here to check the value of i
+  }
+}
+
+console.log(countToTen());
