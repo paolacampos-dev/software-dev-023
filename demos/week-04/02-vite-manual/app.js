8c3e3581869a42df08ff5c64b7ec3a2c5c38814e
Tim Smith <tim.smith2814@gmail.com>
2025-12-08 10:16:19 +0000

    Add vite demos

---
+++ demos/week-04/02-vite-manual/app.js
@@ -0,0 +1,14 @@
+// this is a comment
+const username = "Tim";
+/**
+ *
+ * multi line comments...
+ *
+ * are ugly
+ *
+ *
+ *
+ * but thanksfully it will be removed by vite build
+ *
+ */
+console.log(`Everyone say hi ${username}!`);
