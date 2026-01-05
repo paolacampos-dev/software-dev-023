7045ffa25309142592f7c6835da0a77b1c845b74
Tim Smith <tim.smith2814@gmail.com>
2026-01-05 11:33:53 +0000

    Add react demo

---
+++ demos/week-06/01-react-setup/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  // JavaScript
+  const username = "Software Development 023";
+
+  // HTML (using JSX)
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>This is my first react app</p>
+      <p>Welcome {username}</p>
+      <p>Look, JavaScript! {1 + 2}</p>
+    </div>
+  );
+}
+
+export default App;
