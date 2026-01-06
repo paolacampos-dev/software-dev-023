e7ae7e981e59751964ce802953548dc38bcadda3
Tim Smith <tim.smith2814@gmail.com>
2026-01-06 10:42:20 +0000

    Add styling react demos

---
+++ demos/week-06/04-react-css/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+import Navbar from "./Navbar";
+
+function Header() {
+  return (
+    <header>
+      <h1>I am the Header</h1>
+      <Navbar />
+    </header>
+  );
+}
+
+export default Header;
---
+++ demos/week-06/04-react-css/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </nav>
+  );
+}
+
+export default Navbar;
